74f13dab1d3ab640021662bcf02fb16913b9c923
Pavesh Kanungo <pavesh.k@consultadd.com>
2025-01-22 23:18:23 +0530

    .

--- src/main/java/com/example/demo/repository/CarRepo.java
+++ src/main/java/com/example/demo/repository/CarRepo.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepo extends JpaRepository<Car,Long> {
+    List<Car> findByName(String name);
 }
